fb1df106239488e74b63dd7eacb779371166370c
Gary Green <holegary@gmail.com>
2015-08-18 20:49:21 +0100

    Added dynamic validatorOptions tests.

---
+++ tests/dynamic-options.js
@@ -0,0 +1,37 @@
+var expect = window.chai.expect;
+
+describe('dynamic validatorOptions tests', function() {
+
+	it('should should allow dynamic options', function() {
+
+		var dominar = new Dominar($form = $('<form><input name="a" value="test1"><input name="b" value="test2"></form>'), {
+			a: {
+				rules: 'required'
+			},
+			b: {
+				rules: 'test',
+				validatorOptions: function(options) {
+					if (dominar.getField('a').getValue().length) {
+						options.rules = 'required|min:3|' + options.rules.b;
+					}
+					return options;
+				},
+				customMessages: {
+					min: 'Silly, this should be at least :min characters.'
+				}
+			}
+		});
+
+		expect(dominar.getField('b').getValidationOptions()).to.deep.equal({
+			data: {
+				b: 'test2'
+			},
+			rules: 'required|min:3|test',
+			customMessages: {
+				min: 'Silly, this should be at least :min characters.'
+			}
+		});
+
+	});
+
+});
