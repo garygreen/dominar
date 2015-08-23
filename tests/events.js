372bac320264c9f68fdf832c7dea52a723eda25f
Gary Green <holegary@gmail.com>
2015-08-23 16:51:10 +0100

    Fix event tests

--- tests/events.js
+++ tests/events.js
@@ -4,61 +4,81 @@ describe('event tests', function() {
 
 	this.timeout(100);
 
+	beforeEach(function() {
+		this.dominar = new Dominar($('<form><input name="username" type="text"></form>'), { username: { rules: 'required' } });
+		this.dominar.$form.on('submit', function(event) {
+			event.preventDefault();
+		});
+
+	});
+
+	afterEach(function() {
+		this.dominar.$form.unbind(this.event);
+	});
+
 	it('should trigger a "dominar.init-field" event when field is initialized', function(done) {
 
-		var $form;
-		var dominar = new Dominar($form = $('<form><input name="username" type="text"></form>'), { username: {} });
+		var dominar = this.dominar;
 
-		$form.bind('dominar.init-field', function(event) {
+		dominar.$form.bind('dominar.init-field', function(event) {
 			expect(event.dominar).to.equal(dominar);
 			expect(event.dominarField.$field.attr('name')).to.equal('username');
 			done();
 		});
 
-		dominar.getField('username');
+		this.dominar.getField('username');
+
+		this.event = 'dominar.init-field';
 	});
 
 
 	it('should fire a "dominar.submit" event when form is submitted and allow it to be prevented.', function(done) {
 
-		var $form;
-		var dominar = new Dominar($form = $('<form></form>'));
+		this.dominar.$form.on('submit', function(event) {
+			event.preventDefault();
+		});
 
-		$form.bind('dominar.submit', function(event) {
+		this.dominar.$form.bind('dominar.submit', function(event) {
 			event.preventDefault();
 			done();
 		});
 
-		$form.submit();
+		this.dominar.$form.submit();
+
+		this.event = 'dominar.submit';
 
 	});
 
 
 	it('should fire a "dominar.submit-passed" event when form passed validation and allow it to be prevented.', function(done) {
 
-		var $form;
-		var dominar = new Dominar($form = $('<form></form>'));
+		var dominar = this.dominar;
 
-		$form.bind('dominar.submit-passed', function(event) {
+		var $username = dominar.$form.find('input');
+		$username.val('test');
+
+		dominar.$form.bind('dominar.submit-passed', function(event) {
 			event.preventDefault();
+			$username.val('');
 			done();
 		});
 
-		$form.submit();
+		dominar.$form.submit();
+
+		this.event = 'dominar.submit-passed';
 
 	});
 
 
 	it('should fire a "dominar.submit-failed" event when form fails validation.', function(done) {
 
-		var $form;
-		var dominar = new Dominar($form = $('<form><input name="username" type="text"></form>'), { username: { rules: 'required' } });
-
-		$form.bind('dominar.submit-failed', function(event) {
+		this.dominar.$form.bind('dominar.submit-failed', function(event) {
 			done();
 		});
 
-		$form.submit();
+		this.dominar.$form.submit();
+
+		this.event = 'dominar.submit-failed';
 
 	});
 
