388043df5a85acadc3d8e6406d048a9f6c94a1c3
Gary Green <holegary@gmail.com>
2015-09-03 22:59:13 +0100

    Renamed custom-messages tests to messages.

--- tests/messages.js
+++ tests/messages.js
@@ -1,11 +1,32 @@
 var expect = window.chai.expect;
 
+describe('message tests', function() {
+
+	it('should show error required error message', function() {
+		
+		var dominar = new Dominar($('<form><div class="form-group"><input name="username"></div></form>'), {
+			username: {
+				rules: 'required',
+				feedback: false,
+				message: true
+			}
+		});
+
+		dominar.validateAll();
+
+		expect(dominar.$form.html()).to.contain([
+			'<input name="username">',
+			'<span class="help-block">The username field is required.</span>',
+		].join(''));
+	});
+
+});
+
 describe('custom messages tests', function() {
 
 	it('should allow custom error message', function() {
 
-		var $form;
-		var dominar = new Dominar($form = $('<form><div class="form-group"><input name="username"/></div></form></form>'), {
+		var dominar = new Dominar($('<form><div class="form-group"><input name="username"/></div></form></form>'), {
 			username: {
 				rules: 'required',
 				feedback: false,
@@ -15,8 +36,8 @@ describe('custom messages tests', function() {
 			}
 		});
 
-		dominar.validate($form.find('input'));
-		expect($form.html()).to.equal([
+		dominar.validate(dominar.$form.find('input'));
+		expect(dominar.$form.html()).to.equal([
 			'<div class="form-group has-error">',
 				'<input name="username">',
 				'<span class="help-block">username field is required you silly billy!</span>',
@@ -49,3 +70,4 @@ describe('custom messages tests', function() {
 	});
 
 });
+
